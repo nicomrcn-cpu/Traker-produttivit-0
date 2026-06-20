9140cfa8a21bff25331ef487e7033f99ae9d6a8e
nicomrcn-cpu <nico.mrcn@gmail.com>
2026-06-20 20:37:23 +0200

    Update firebase.ts
    correzione chiavi

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDeHw-f7RCpJD67biQz2V3xD",
+  apiKey: "AIzaSyDeHw-f7RCpJD67biQz2V3xD48r8QqVhds",
   authDomain: "traker-produttivita.firebaseapp.com",
   projectId: "traker-produttivita",
   storageBucket: "traker-produttivita.firebasestorage.app",
   messagingSenderId: "455133424401",
-  appId: "1:455133424401:web:0fd1a42cd75",
+  appId: "1:455133424401:web:0fd1a42cd75f066de03ab0",
   measurementId: "G-SW221KG3VG"
 };
 
