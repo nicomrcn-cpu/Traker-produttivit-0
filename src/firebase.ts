567030d5a7e3f1422dbb8bd00b6a9d6bcff0ad9e
nicomrcn-cpu <nico.mrcn@gmail.com>
2026-06-21 13:53:18 +0200

    Update firebase.ts
    Modifiche all'app

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,11 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDeHw-f7RCpJD67biQz2V3xD48r8QqVhds",
   authDomain: "traker-produttivita.firebaseapp.com",
@@ -12,6 +16,6 @@ const firebaseConfig = {
   measurementId: "G-SW221KG3VG"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
